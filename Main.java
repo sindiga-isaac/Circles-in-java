9193ba21e05d547f8e54b37922298b82b286784f
isaac <59864350+sindiga1@users.noreply.github.com>
2021-10-16 10:14:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-publ
+public class Main{
+    public static void main(String[]args){
+        final double Math.Pi=22/7;
+        float r=7.5f;
+        System.out.println("Radious of the circle is:"+r);
+        double area=Pi*(r*r);
+        System.out.println("Area is:"+area);
+        double cir=2*Pi*r;
+        System.out.println("Circumference is:"+cir);
+    }
+}
