83a30f00f99089203b28722d159388fa837fd407
isaac <59864350+sindiga1@users.noreply.github.com>
2021-10-19 07:54:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main{
-    private static final double r=7.5;
     public static void main(String[]args){
-        double cir=2*Math.PI*r;
-        System.out.println("Perimeter of the circle is:"+cir);
-        double area=Math.PI*(r*r);
-        System.out.println("Area of circle is:"+area);
-       }
+        String binaryString="1010";
+        int decimal=Integer.parseInt(binaryString,2);
+        System.out.println(decimal);
+    }
 }
